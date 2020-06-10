0585c1164951f2fe34e26467e3730d3b3f9fc092
Brenda Pereira <brendacamara@outlook.com>
2020-06-10 17:46:44 -0300

    Create conceitos.js

---
+++ objetos/conceitos.js
@@ -0,0 +1,27 @@
+//NÃO EXECUTAVEL
+
+//procedural 
+function procedimento(valor1,valor2){
+    // ....
+}
+procedimento(valor1,valor2)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento()
+
+/*conceitos
+1-abstração: observar objetos do mundo real e simplifica-los e traduzir para o sistema
+2-encapsulamento: detalhes complexos de implementação escondidos, interface simples de ususario
+3-herença:É-UM->(é um animal, é alguma coisa) recebe atributos e comportamento dos pais(priorize a composição(objetos especificos que se relacionam) ao inves da herença)
+4-polimorfismo: conceito generalista-> celular, a partir do celular ele pode ser um motorola,um iphone e cada um com suas caracteristica
+*/
+
+
