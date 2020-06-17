2e368a216b9ce4d3faacb2f97f836b21c2257ee4
Brenda Pereira <brendacamara@outlook.com>
2020-06-17 19:47:31 -0300

    global exemple

---
+++ node/global.js
@@ -0,0 +1,8 @@
+//console.log(global)
+
+global.Meuapp = Object.freeze({
+    saudacao(){
+        return 'eu vou estar la'
+    },
+    nome: 'app Julius'
+})
---
+++ node/globalCliente.js
@@ -0,0 +1,7 @@
+require('./global')
+
+console.log(Meuapp.saudacao())
+
+
+Meuapp.nome = 'Eiita'
+console.log(Meuapp.nome)
