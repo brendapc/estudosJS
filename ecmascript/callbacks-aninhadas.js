3c558bcb28cfdd63b2a66ad07cb964f753003117
Brenda Pereira <brendacamara@outlook.com>
2020-06-23 10:59:47 -0300

    Create callbacks-aninhadas.js

---
+++ ecmascript/callbacks-aninhadas.js
@@ -0,0 +1,33 @@
+//sem promise
+const http = require('http')
+
+const getTurma = (letra, callback)=>{
+    const url = `http://files.cod3r.com.br/curso-js/turmas${letra}.json`
+    http.get(url,res =>{
+        let resultado = ''
+
+        res.on('data', dados=>{
+            resultado += dados
+        })
+        res.on('end', () =>{
+            callback(JSON.parse(resultado))
+        })
+    })
+}
+
+let nomes = []  //calback hell (evoid)
+getTurma('A', alunos => { 
+    nomes = nomes.concat(alunos.map(a=> `A: ${a.nomes}`))
+    getTurma('B', alunos=>{
+        nomes = nomes.concat(alunos.map(a=> `B ${b.nome}`))
+        getTurma('C', alunos =>{
+            nomes = nomes.concat(alunos.map(a=> `C ${c.nome}`))
+            console.log(nomes)
+        })
+    })
+
+
+
+})
+
+
