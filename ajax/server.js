a166f0165849f838df5b42752d03480b7e5d95df
Brenda Pereira <brendacamara@outlook.com>
2020-07-18 18:01:58 -0300

    Update server.js

--- ajax/server.js
+++ ajax/server.js
@@ -6,5 +6,27 @@ app.use(express.static('.')) /* dentro da pasta atual sirva os arquivos estátic
 app.use(bodyParser.urlencoded({extended: true})) /* se vier dados a partir de um submit de um formulário le os dados e transforma em objeto*/
 app.use(bodyParser.json()) /* se vier json, transforme em objeto */
 
-app.get('/teste', (req,res) => res.send('ok')) /* middleware chamado a partir de uma req get */
+/* app.get('/teste', (req,res) => res.send('ok')) middleware chamado a partir de uma req get */
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({ /* personalizar de salvamento de arquivos: onde e com que nome (para evitar sobreescrita) */
+    destination: function (req,file,callback){
+        callback(null, './upload')
+    },
+    filename: function (req,file,callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req,res)=>{
+    upload(req,res,err =>{
+        if(err){
+            return res.send('ocorreu um erro'+err)
+        }
+        res.end('concluido com sucesso')
+    })
+})
 app.listen(8080, ()=> console.log('executando...'))
