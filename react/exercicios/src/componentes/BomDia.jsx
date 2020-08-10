8e5e027481d98db6be3a5a16792b7146c1939b8f
Brenda Pereira <brendacamara@outlook.com>
2020-08-10 10:10:15 -0300

    components tags rules

--- react/exercicios/src/componentes/BomDia.jsx
+++ react/exercicios/src/componentes/BomDia.jsx
@@ -1,3 +1,8 @@
 import React from 'react';
 
-export default (props)=> <h1> Bom Dia {props.nome}, idade {props.idade} </h1>
+export default (props)=> 
+/*  não pode retornar duas tags, mas pode retornar uma tag com varias dentro */
+    <div>
+        <h1> Bom Dia {props.nome}</h1>
+        <h2>idade {props.idade} </h2>
+    </div>
