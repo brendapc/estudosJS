e915cd84c2340a2d14cedd75cece9e2156657de9
Brenda Pereira <brendacamara@outlook.com>
2020-08-10 10:12:00 -0300

    React.Fragment

--- react/exercicios/src/componentes/BomDia.jsx
+++ react/exercicios/src/componentes/BomDia.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default (props)=> 
 /*  não pode retornar duas tags, mas pode retornar uma tag com varias dentro */
-    <div>
+/* usando react.fragment para "enrolar" as tags sem quebrar o layout com a div */
+    <React.Fragment> 
         <h1> Bom Dia {props.nome}</h1>
         <h2>idade {props.idade} </h2>
-    </div>
+    </React.Fragment> 
