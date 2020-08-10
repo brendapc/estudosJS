fb5ac7531cbeac0b212565fa3341583069553c61
Brenda Pereira <brendacamara@outlook.com>
2020-08-10 10:35:35 -0300

    import/export

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 
 /* componentes não podem ter letra minuscula para que o react possa diferencia-los de tags padrão */
 /* import Primeiro from './componentes/Primeiro' */
-import BomDia from './componentes/BomDia'
-ReactDOM.render(<BomDia nome="ana" idade="10"/>, document.getElementById('root'))
+/* import BomDia from './componentes/BomDia'*/
+
+import Multi from './componentes/MultiplosComponentes'
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Gabriela"/>
+        <Multi.BoaNoite nome="julia"/>
+    </div>
+    , document.getElementById('root'))
