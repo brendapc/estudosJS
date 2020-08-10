e0b98407efbf4ec6ff4c01dfe4f903059934e17a
Brenda Pereira <brendacamara@outlook.com>
2020-08-10 10:07:42 -0300

    props

---
+++ react/exercicios/src/componentes/BomDia.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default (props)=> <h1> Bom Dia {props.nome}, idade {props.idade} </h1>
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 /* componentes não podem ter letra minuscula para que o react possa diferencia-los de tags padrão */
 import Primeiro from './componentes/Primeiro'
-
-ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+import BomDia from './componentes/BomDia'
+ReactDOM.render(<BomDia nome="ana" idade="10"/>, document.getElementById('root'))
