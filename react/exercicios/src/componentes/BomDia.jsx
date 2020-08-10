452d65b4838a604f9e38969869c4837d9063d96e
Brenda Pereira <brendacamara@outlook.com>
2020-08-10 10:16:46 -0300

    components returning arrays

--- react/exercicios/src/componentes/BomDia.jsx
+++ react/exercicios/src/componentes/BomDia.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 export default (props)=> 
 /*  não pode retornar duas tags, mas pode retornar uma tag com varias dentro */
 /* usando react.fragment para "enrolar" as tags sem quebrar o layout com a div */
-    <React.Fragment> 
+/*     <React.Fragment> 
         <h1> Bom Dia {props.nome}</h1>
         <h2>idade {props.idade} </h2>
-    </React.Fragment> 
+    </React.Fragment>  
+-----------------------------------    
+*/ /* returning arrays */
+    [
+        <h1 key='h1'> Bom Dia {props.nome}</h1>,
+        <h2 key='h2'>idade {props.idade} </h2>
+    ]
