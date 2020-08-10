0719a4121d9d93ea51e669a109ea62cf7bb347fd
Brenda Pereira <brendacamara@outlook.com>
2020-08-10 10:01:56 -0300

    components #1

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/* componentes não podem ter letra minuscula para que o react possa diferencia-los de tags padrão */
 import Primeiro from './componentes/Primeiro'
 
 ReactDOM.render(<Primeiro/>, document.getElementById('root'))
