761310601804ddfa5af57ec0b35ec8ab3e82e1fc
Brenda Pereira <brendacamara@outlook.com>
2020-08-10 10:14:04 -0300

    another way of wrapping tags

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 /* componentes não podem ter letra minuscula para que o react possa diferencia-los de tags padrão */
-import Primeiro from './componentes/Primeiro'
+/* import Primeiro from './componentes/Primeiro' */
 import BomDia from './componentes/BomDia'
 ReactDOM.render(<BomDia nome="ana" idade="10"/>, document.getElementById('root'))
