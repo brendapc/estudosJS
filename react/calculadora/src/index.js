45726f1fc9de81ec4fe1c312b374aaf4bed3e053
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-08-13 09:09:11 -0300

    add buttons

--- react/calculadora/src/index.js
+++ react/calculadora/src/index.js
@@ -5,8 +5,8 @@ import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <div>
-    <h1>Calculator</h1>
+  <div className="container">
+    <h1>Calculator with React.js</h1>
     <Calculator />
   </div>
   ,document.getElementById('root'));
